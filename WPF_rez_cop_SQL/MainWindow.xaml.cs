c73dac9f9d7fada550644ed1ddaeb39a7b4df825
Helld0n <ya06max@gmail.com>
2026-02-22 21:35:24 +0500

    v 1.1
    Доделал интерфейс

---
+++ WPF_rez_cop_SQL/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace WPF_rez_cop_SQL
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
